512724b99d6c1c303c73330c9f859157a2f78509
felps18 <felps18.08@gmail.com>
2023-10-28 15:01:03 -0300

    fix emotespells change command orange

--- sources/game.cpp
+++ sources/game.cpp
@@ -4790,6 +4790,8 @@ bool Game::internalCreatureSay(Creature* creature, MessageClasses type, const st
                         tmpPlayer->sendCreatureSay(creature, MSG_SPEAK_YELL, text, &destPos, statementId);
 					else if(std::stoi(value) == 2) {
 						//tmpPlayer->sendCreatureSay(creature, MSG_NONE, text, &destPos, statementId); //no message needed
+					}else{
+                        tmpPlayer->sendCreatureSay(creature, type, text, &destPos, statementId);
 					}
 				}
 				else
