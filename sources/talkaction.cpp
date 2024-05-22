622642898aa3c5b260f421151fea35acee931137
Felipe Forte <51446231+FeTads@users.noreply.github.com>
2024-05-22 17:42:05 -0300

    Update talkaction.cpp add trim function

--- sources/talkaction.cpp
+++ sources/talkaction.cpp
@@ -1248,6 +1248,14 @@ bool TalkAction::diagnostics(Creature* creature, const std::string&, const std::
 	return true;
 }
 
+std::string TalkAction::trim(const std::string& str) {
+    size_t first = str.find_first_not_of(' ');
+    if (std::string::npos == first)
+        return str;
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
+}
+
 bool TalkAction::isInputValid(const std::string& input) {
     boost::regex validInputRegex("^[a-zA-Z0-9,! ]*$");
     return boost::regex_match(input, validInputRegex);
